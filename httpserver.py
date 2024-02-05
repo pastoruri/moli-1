bc4dff47cbf2371c5063277365dde375e1bc16b5
pastoruri <40170714+pastoruri@users.noreply.github.com>
2024-02-05 15:44:49 -0500

    Update httpserver.py

--- httpserver.py
+++ httpserver.py
@@ -3,20 +3,37 @@ import ssl
 
 class RedirectHandler(BaseHTTPRequestHandler):
     def do_GET(self):
+        self.redirect()
+
+    def do_POST(self):
+        self.redirect()
+    
+    def do_PUT(self):
+        self.redirect()
+    
+    def do_DELETE(self):
+        self.redirect()
+
+    def redirect(self):
+        # Cambia "localhost" por tu dominio o dirección IP específica si es necesario
+        new_path = f'http://34.125.51.194:8000{self.path}'
         self.send_response(301)
-        new_path = 'http://%s%s' % (self.headers['Host'], self.path)
         self.send_header('Location', new_path)
         self.end_headers()
 
-server_address = ('localhost', 443)
-httpd = HTTPServer(server_address, RedirectHandler)
+def run(server_class=HTTPServer, handler_class=RedirectHandler, port=443):
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    
+    # Configura aquí tu certificado y clave
+    httpd.socket = ssl.wrap_socket(httpd.socket,
+                                   server_side=True,
+                                   certfile='path/to/cert.pem',
+                                   keyfile='path/to/key.pem',
+                                   ssl_version=ssl.PROTOCOL_TLS)
 
-# Ruta a los certificados SSL para HTTPS
-httpd.socket = ssl.wrap_socket(httpd.socket,
-                               server_side=True,
-                               certfile='cert.pem',
-                               keyfile='key.pem',
-                               ssl_version=ssl.PROTOCOL_TLS)
+    print(f'Servidor corriendo en https://0.0.0.0:{port} y redirigiendo a http://localhost:8000')
+    httpd.serve_forever()
 
-print('Servidor corriendo en https://localhost:443')
-httpd.serve_forever()
+if __name__ == "__main__":
+    run()
